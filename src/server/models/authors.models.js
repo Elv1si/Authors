f7d56ade3d4bb23cd28baf3d3950aa3eb8b87906
Elvis Tashi <123761180+Elv1si@users.noreply.github.com>
2023-05-26 22:20:45 +0200

    Add files via upload

---
+++ src/server/models/authors.models.js
@@ -0,0 +1,9 @@
+const AuthorsSchema = {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      minLength: [3, "Name must be at least 3 characters"],
+    },
+  };
+  
+  module.exports = mongoose.model("Author", AuthorsSchema);
